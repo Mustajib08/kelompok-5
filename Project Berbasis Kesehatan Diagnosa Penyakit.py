a5538031989fa13084ce8d17b74020b83fadd607
Ajib <75076889+Mustajib08@users.noreply.github.com>
2020-12-17 22:53:03 +0700

    Lanjutan dari deva

--- Project Berbasis Kesehatan Diagnosa Penyakit.py
+++ Project Berbasis Kesehatan Diagnosa Penyakit.py
@@ -16,3 +16,22 @@ while pilihan == "Y":
   print("3.Diare ")
   print("4.Kelelahan & Lemas ")
   diag1 = input(" Jawab Y/N : ")
+
+  if diag1 == "Y":
+    print("\n Apakah Anda Juga Merasakan Gejala Ini :")
+    print("5.Ruam Merah ")
+    print("6.Mual Muntah")
+
+    diag2 = input(" Jawab Y/N : ")
+
+    if diag2 == "Y" :
+      print("\n Hi, "+nama+" Hasil Awal Diagnosa Kamu Adalah :")
+      print(" Gejala Tifus,Segeralah Kedokter !!!")
+
+    elif diag2 == "N":
+      print("\n Apakah Anda Juga Mersakan Gejala Berikut Ini :")
+      print("1.Demam/Suhu Badan Tinggi")
+      print("2.Sakit Kepla")
+      print("3.Hilangnya Indra Perasa/Penciuman")
+      print("4.Nyeri Tengkorokan ")
+      diag3 = input(" Jawab Y/N : ")
